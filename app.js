542992aade4c5a8e723a53acf95539643ba061b1
Bryant Richards <dragonseyeimages@gmail.com>
2023-08-22 07:25:38 -0500

    Object Types

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-var add = function (n1, n2, showResult, phrase) {
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-    //   throw new Error('Incorrect input!')
-    // }
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
+// const person: {
+//   name: string
+//   age: number
+// } = {
+var person = {
+    name: 'Kirk',
+    age: 30,
 };
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-add(number1, number2, printResult, resultPhrase);
+console.log(person.name);
