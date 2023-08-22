46bf5cce78c131745aa8188705f955329f6a2ce9
Bryant Richards <dragonseyeimages@gmail.com>
2023-08-22 07:31:31 -0500

    Arrays Types

--- app.ts
+++ app.ts
@@ -5,6 +5,15 @@
   const person = {
   name: 'Kirk',
   age: 30,
+  hobbies: ['Exploring', 'Women']
 }
 
+let favoriteActivities: string[]
+favoriteActivities = ['Women']
+
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase())
+  // console.log(hobby.map()) // !!! ERROR !!!
+}
