13096b1faf82d8e74c791f8def06864f168b221a
Bryant Richards <dragonseyeimages@gmail.com>
2023-08-23 07:04:02 -0500

    Working with Tuples

--- app.ts
+++ app.ts
@@ -2,11 +2,22 @@
 //   name: string
 //   age: number
 // } = {
-  const person = {
+  const person: {
+    name: string
+    age: number
+    hobbies: string[]
+    role: [number, string] // Tuple
+  } = {
   name: 'Kirk',
   age: 30,
-  hobbies: ['Exploring', 'Women']
+  hobbies: ['Exploring', 'Women'],
+  role: [2, 'captain']
 }
+
+// person.role.push('doctor')
+// person.role[1] = 10 // !!! ERROR !!!
+
+// person.role = [0, 'doctor', 'Ensign'] !!! ERROR !!!
 
 let favoriteActivities: string[]
 favoriteActivities = ['Women']
