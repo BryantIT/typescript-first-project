addaa0fbd3a2b6c1829d417642f895483b3fe73a
Bryant Richards <dragonseyeimages@gmail.com>
2023-08-24 07:16:48 -0500

    Type Aliases

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var combine = function (input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+    if ((typeof input1 === 'number' && typeof input2 === 'number') ||
+        resultConversion === 'as-number') {
         result = +input1 + +input2;
     }
     else {
