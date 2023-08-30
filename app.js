f4cfb1d9640bea78c40ab8141b55e8a9159f808c
Bryant Richards <dragonseyeimages@gmail.com>
2023-08-30 07:11:05 -0500

    Compile entire project

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-var userInput;
-var userName;
+"use strict";
+let userInput;
+let userName;
 userInput = 5;
 userInput = 'Kirk';
 if (typeof userInput === 'string') {
     userName = userInput;
 }
 // userName = userInput // Type 'unknown' is not assignable to type 'string'
-var generateError = function (message, code) {
+const generateError = (message, code) => {
     throw { message: message, errorCode: code };
 };
 generateError('An error occurred!', 500);
