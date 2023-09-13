7011663742ecb831a84083e82f08988638621e9e
Bryant Richards <dragonseyeimages@gmail.com>
2023-09-13 07:27:21 -0500

    private and public modifiers

--- src/app.js
+++ src/app.js
@@ -1,8 +1,23 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.employees = [];
         this.name = n;
+    }
+    describe() {
+        console.log('Department: ' + this.name);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
     }
 }
 const command = new Department('Command');
-console.log(command);
+command.addEmployee('Spock');
+command.addEmployee('Bones');
+command.describe();
+// const commandCopy = { name: 'second', describe: command.describe }
+// commandCopy.describe()
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string
+  private employees: string[] = []
 
   constructor(n: string) {
     this.name = n
@@ -8,12 +9,24 @@ class Department {
   describe(this: Department) {
     console.log('Department: ' + this.name)
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee)
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length)
+    console.log(this.employees)
+  }
 }
 
 const command = new Department('Command')
 
+command.addEmployee('Spock')
+command.addEmployee('Bones')
+
 command.describe()
 
-const commandCopy = { name: 'second', describe: command.describe }
+// const commandCopy = { name: 'second', describe: command.describe }
 
-commandCopy.describe()
+// commandCopy.describe()
