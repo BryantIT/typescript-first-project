6c76b8306ea1b01ac4de61c0c168d1aad027a41c
Bryant Richards <dragonseyeimages@gmail.com>
2023-11-07 06:48:23 -0600

    first interface

--- src/app.js
+++ src/app.js
@@ -1,58 +1,10 @@
 "use strict";
-class Department {
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
-        // private readonly id: string
-        // private name: string
-        this.employees = [];
-        // this.id = id
-        // this.name = n
+let user1;
+user1 = {
+    name: 'Kirk',
+    age: 30,
+    greet(phrase) {
+        console.log(phrase + ' ' + this.name);
     }
-    describe() {
-        console.log(`Department (${this.id}): ${this.name}`);
-    }
-    addEmployee(employee) {
-        this.employees.push(employee);
-    }
-    printEmployeeInformation() {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    }
-}
-class ITDepartment extends Department {
-    constructor(id, admins) {
-        super(id, 'IT');
-        this.admins = admins;
-    }
-}
-class AccountingDepartment extends Department {
-    constructor(id, reports) {
-        super(id, 'Accounting');
-        this.reports = reports;
-    }
-    addEmployee(employee) {
-        if (employee === 'Kirk') {
-            return;
-        }
-        this.employees.push(employee);
-    }
-    addReport(text) {
-        this.reports.push(text);
-    }
-    printReports() {
-        console.log(this.reports);
-    }
-}
-const it = new ITDepartment('d1', ['Kirk']);
-const command = new Department('a2', 'Command');
-const accounting = new AccountingDepartment('a1', []);
-accounting.addReport('Something went wrong...');
-accounting.addEmployee('Kirk');
-accounting.addEmployee('Scotty');
-accounting.printReports();
-command.addEmployee('Spock');
-command.addEmployee('Bones');
-command.describe();
-// const commandCopy = { name: 'second', describe: command.describe }
-// commandCopy.describe()
+};
+user1.greet('Hi there - I am');
--- src/app.ts
+++ src/app.ts
@@ -1,72 +1,18 @@
-class Department {
-  // private readonly id: string
-  // private name: string
-  protected employees: string[] = []
+interface Person {
+  name: string;
+  age: number;
 
-  constructor(private readonly id: string, public name: string) {
-    // this.id = id
-    // this.name = n
-  }
+  greet(phrase: string): void;
+}
 
-  describe(this: Department) {
-    console.log(`Department (${this.id}): ${this.name}`)
-  }
+let user1: Person;
 
-  addEmployee(employee: string) {
-    this.employees.push(employee)
-  }
-
-  printEmployeeInformation() {
-    console.log(this.employees.length)
-    console.log(this.employees)
+user1 = {
+  name: 'Kirk',
+  age: 30,
+  greet(phrase: string) {
+    console.log(phrase + ' ' + this.name);
   }
 }
 
-class ITDepartment extends Department {
-  admins: string[]
-  constructor(id: string, admins: string[]) {
-    super(id, 'IT')
-    this.admins = admins
-  }
-}
-
-class AccountingDepartment extends Department {
-  constructor(id: string, private reports: string[]) {
-    super(id, 'Accounting')
-  }
-
-  addEmployee(employee: string): void {
-    if (employee === 'Kirk') {
-      return
-    }
-    this.employees.push(employee)
-  }
-
-  addReport(text: string) {
-    this.reports.push(text)
-  }
-
-  printReports() {
-    console.log(this.reports)
-  }
-}
-
-const it = new ITDepartment('d1', ['Kirk'])
-
-const command = new Department('a2', 'Command')
-
-const accounting = new AccountingDepartment('a1', [])
-
-accounting.addReport('Something went wrong...')
-accounting.addEmployee('Kirk')
-accounting.addEmployee('Scotty')
-accounting.printReports()
-
-command.addEmployee('Spock')
-command.addEmployee('Bones')
-
-command.describe()
-
-// const commandCopy = { name: 'second', describe: command.describe }
-
-// commandCopy.describe()
+user1.greet('Hi there - I am');
