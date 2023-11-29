d5165e86c36c88f71cda57939635670c1261c32c
Bryant Richards <dragonseyeimages@gmail.com>
2023-11-29 07:22:19 -0600

    another generic function

--- src/app.js
+++ src/app.js
@@ -11,3 +11,13 @@ const merge = (objA, objB) => {
     return Object.assign(Object.assign({}, objA), objB);
 };
 const mergedObj = merge({ name: 'Kirk' }, { age: 30 });
+const countAndDescribe = (element) => {
+    let descriptionText = 'Got no value.';
+    if (element.length === 1) {
+        descriptionText = 'Got 1 element.';
+    }
+    else if (element.length > 1) {
+        descriptionText = 'Got ' + element.length + ' elements.';
+    }
+    return [element, descriptionText];
+};
--- src/app.ts
+++ src/app.ts
@@ -13,3 +13,13 @@ const merge = <T extends object, U extends object>(objA: T, objB: U) => {
 }
 
 const mergedObj = merge({ name: 'Kirk' }, { age: 30 })
+
+const countAndDescribe = <T extends { length: number }>(element: T): [T, string] => {
+  let descriptionText = 'Got no value.'
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element.'
+  } else if (element.length > 1) {
+    descriptionText = 'Got ' + element.length + ' elements.'
+  }
+  return [element, descriptionText]
+}
