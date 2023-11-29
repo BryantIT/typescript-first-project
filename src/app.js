1557d852731a05a03a4449716ed9919c92c7a899
Bryant Richards <dragonseyeimages@gmail.com>
2023-11-29 07:12:21 -0600

    creating a generic

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 "use strict";
-const names = ['Kirk', 'Spock', 'McCoy']; // same as string[]
-// const names: Array<string | number> = ['Kirk', 'Spock', 'McCoy'] // can also be a union type
-// names[0].split('')
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('This is done!');
-    }, 2000);
-});
+// const names: Array<string> = ['Kirk', 'Spock', 'McCoy'] // same as string[]
+// // const names: Array<string | number> = ['Kirk', 'Spock', 'McCoy'] // can also be a union type
+// // names[0].split('')
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('This is done!')
+//   }, 2000)
+// })
+const merge = (objA, objB) => {
+    return Object.assign(Object.assign({}, objA), objB);
+};
+const mergedObj = merge({ name: 'Kirk' }, { age: 30 });
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
-const names: Array<string> = ['Kirk', 'Spock', 'McCoy'] // same as string[]
-// const names: Array<string | number> = ['Kirk', 'Spock', 'McCoy'] // can also be a union type
-// names[0].split('')
+// const names: Array<string> = ['Kirk', 'Spock', 'McCoy'] // same as string[]
+// // const names: Array<string | number> = ['Kirk', 'Spock', 'McCoy'] // can also be a union type
+// // names[0].split('')
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve('This is done!')
-  }, 2000)
-})
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve('This is done!')
+//   }, 2000)
+// })
+
+const merge = <T, U>(objA: T, objB: U) => {
+  return { ...objA, ...objB }
+}
+
+const mergedObj = merge({ name: 'Kirk' }, { age: 30 })
