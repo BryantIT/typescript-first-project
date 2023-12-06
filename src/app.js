f004dc0d01b6e72bf08c7eda499e5e23c07e98d6
Bryant Richards <dragonseyeimages@gmail.com>
2023-12-06 07:15:14 -0600

    first decorator

--- src/app.js
+++ src/app.js
@@ -1,57 +1,22 @@
 "use strict";
-// const names: Array<string> = ['Kirk', 'Spock', 'McCoy'] // same as string[]
-// // const names: Array<string | number> = ['Kirk', 'Spock', 'McCoy'] // can also be a union type
-// // names[0].split('')
-// const promise: Promise<string> = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve('This is done!')
-//   }, 2000)
-// })
-const merge = (objA, objB) => {
-    return Object.assign(Object.assign({}, objA), objB);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-const mergedObj = merge({ name: 'Kirk' }, { age: 30 });
-const countAndDescribe = (element) => {
-    let descriptionText = 'Got no value.';
-    if (element.length === 1) {
-        descriptionText = 'Got 1 element.';
-    }
-    else if (element.length > 1) {
-        descriptionText = 'Got ' + element.length + ' elements.';
-    }
-    return [element, descriptionText];
+const Logger = (constructor) => {
+    console.log('Logging...');
+    console.log(constructor);
 };
-const extractAndConvert = (obj, key) => {
-    return 'Value: ' + obj[key];
-};
-class DataStorage {
+let Person = class Person {
     constructor() {
-        this.data = [];
+        this.name = 'Kirk';
+        console.log('Creating person object...');
     }
-    addItem(item) {
-        this.data.push(item);
-    }
-    removeItem(item) {
-        if (this.data.indexOf(item) === -1) {
-            return;
-        }
-        this.data.splice(this.data.indexOf(item), 1);
-    }
-    getItems() {
-        return [...this.data];
-    }
-}
-const textStorage = new DataStorage();
-textStorage.addItem('Kirk');
-textStorage.addItem('Spock');
-textStorage.removeItem('Kirk');
-console.log(textStorage.getItems());
-const numberStorage = new DataStorage();
-const createCourseGoal = (title, description, date) => {
-    let courseGoal = {};
-    courseGoal.title = title;
-    courseGoal.description = description;
-    courseGoal.completeUntil = date;
-    return courseGoal;
 };
-const names = ['Kirk', 'Spock'];
+Person = __decorate([
+    Logger
+], Person);
+const person = new Person();
+console.log(person.name);
--- src/app.ts
+++ src/app.ts
@@ -1,74 +1,17 @@
-// const names: Array<string> = ['Kirk', 'Spock', 'McCoy'] // same as string[]
-// // const names: Array<string | number> = ['Kirk', 'Spock', 'McCoy'] // can also be a union type
-// // names[0].split('')
-
-// const promise: Promise<string> = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve('This is done!')
-//   }, 2000)
-// })
-
-const merge = <T extends object, U extends object>(objA: T, objB: U) => {
-  return { ...objA, ...objB }
+const Logger = (constructor: Function) => {
+  console.log('Logging...')
+  console.log(constructor)
 }
 
-const mergedObj = merge({ name: 'Kirk' }, { age: 30 })
+@Logger
+class Person {
+  name = 'Kirk'
 
-const countAndDescribe = <T extends { length: number }>(element: T): [T, string] => {
-  let descriptionText = 'Got no value.'
-  if (element.length === 1) {
-    descriptionText = 'Got 1 element.'
-  } else if (element.length > 1) {
-    descriptionText = 'Got ' + element.length + ' elements.'
-  }
-  return [element, descriptionText]
-}
-
-const extractAndConvert = <T extends object, U extends keyof T>(obj: T, key: U) => {
-  return 'Value: ' + obj[key]
-}
-
-class DataStorage<T extends string | number | boolean> {
-  private data: T[] = []
-
-  addItem(item: T) {
-    this.data.push(item)
-  }
-
-  removeItem(item: T) {
-    if (this.data.indexOf(item) === -1) {
-      return
-    }
-    this.data.splice(this.data.indexOf(item), 1)
-  }
-
-  getItems() {
-    return [...this.data]
+  constructor() {
+    console.log('Creating person object...')
   }
 }
 
-const textStorage = new DataStorage<string>()
-textStorage.addItem('Kirk')
-textStorage.addItem('Spock')
-textStorage.removeItem('Kirk')
-console.log(textStorage.getItems())
+const person = new Person()
 
-const numberStorage = new DataStorage<number>()
-
-// const objStorage = new DataStorage<object>()
-
-interface CourseGoal {
-  title: string
-  description: string
-  completeUntil: Date
-}
-
-const createCourseGoal = (title: string, description: string, date: Date): CourseGoal => {
-  let courseGoal: Partial<CourseGoal> = {}
-  courseGoal.title = title
-  courseGoal.description = description
-  courseGoal.completeUntil = date
-  return courseGoal as CourseGoal
-}
-
-const names: Readonly<string[]> = ['Kirk', 'Spock']
+console.log(person.name)
