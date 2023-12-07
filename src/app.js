818292f5de815e2c496aa4b6f42f7a3bba211d16
Bryant Richards <dragonseyeimages@gmail.com>
2023-12-07 07:10:29 -0600

    accessor & Parameter Decorators

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 const Logger = (logString) => {
     return (constructor) => {
         console.log(logString);
@@ -43,6 +46,18 @@ const Log2 = (target, name, descriptor) => {
     console.log(name);
     console.log(descriptor);
 };
+const Log3 = (target, name, descriptor) => {
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+};
+const Log4 = (target, name, position) => {
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+};
 class Product {
     set price(val) {
         if (val > 0) {
@@ -66,3 +81,7 @@ __decorate([
 __decorate([
     Log2
 ], Product.prototype, "price", null);
+__decorate([
+    Log3,
+    __param(0, Log4)
+], Product.prototype, "getPriceWithTax", null);
--- src/app.ts
+++ src/app.ts
@@ -44,6 +44,20 @@ const Log2 = (target: any, name: string, descriptor: PropertyDescriptor) => {
 
 }
 
+const Log3 = (target: any, name: string | Symbol, descriptor: PropertyDescriptor) => {
+  console.log('Method decorator!')
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
+const Log4 = (target: any, name: string | Symbol, position: number) => {
+  console.log('Parameter decorator!')
+  console.log(target)
+  console.log(name)
+  console.log(position)
+}
+
 class Product {
   @Log
   title: string
@@ -63,7 +77,8 @@ class Product {
     this._price = p
   }
 
-  getPriceWithTax(tax: number) {
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
     return this._price * (1 + tax)
   }
 }
